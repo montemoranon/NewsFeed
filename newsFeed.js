aaad445b61d91895cde3eebf3cba806b0053e41d
Nicholas Montemorano <nmm9422@rit.edu>
2018-03-28 22:16:09 -0400

    add stylesheet

--- newsFeed.js
+++ newsFeed.js
@@ -6,7 +6,12 @@ class NewsItem {
     }
 }
 
-function generateESPNItems() {
+function onPageLoad() {
+    generateXMLHTTPRequest(generateESPNItems, 'http://www.espn.com/espn/rss/nfl/news');
+    // generateXMLHTTPRequest(generateCNNItems, 'http://wxdata.weather.com/wxdata/weather/rss/local/14564');
+}
+
+function generateXMLHTTPRequest(func, url) {
     var xmlhttp = null;
 
     if (window.XMLHttpRequest) {
@@ -17,24 +22,33 @@ function generateESPNItems() {
         // code for older versions of IE
         xmlhttp = new ActiveXObject();
     }
-    
+
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            var xmlDoc = xmlhttp.responseXML;
-            var items = xmlDoc.getElementsByTagName("item");
-            var parentList = document.getElementById("ESPNList");
-
-            for (var i = 0; i < items.length; i++) {
-                var listItem = document.createElement("li");
-                listItem.textContent = items[i].childNodes[0].textContent;
-                parentList.appendChild(listItem);
-            }
+            func(xmlhttp);
         }
     }
 
-    xmlhttp.open("GET", 'http://www.espn.com/espn/rss/nfl/news', true);
+    xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
 
-function generateCNNItems() {
+function generateESPNItems(xmlhttp) {
+    var xmlDoc = xmlhttp.responseXML;
+    var items = xmlDoc.getElementsByTagName("item");
+    var parentList = document.getElementById("ESPNList");
+
+    for (var i = 0; i < items.length; i++) {
+        var listItem = document.createElement("li");
+
+        listItem.textContent = items[i].childNodes[0].textContent;
+        listItem.classList.add("list-group-item");
+        listItem.classList.add("ESPNListItem");
+
+        parentList.appendChild(listItem);
+    }
+}
+
+function generateCNNItems(xmlhttp) {
+    console.log(xmlhttp.responseXML.getElementsByName())
 }
