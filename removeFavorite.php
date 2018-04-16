b55cea829b5ccba7d437a4573b1c68c78f9dcbf3
Nick Montemorano <nmm9422@rit.edu>
2018-04-16 01:11:01 -0400

    Implement remove single favorite

---
+++ removeFavorite.php
@@ -0,0 +1,10 @@
+<?php
+
+require('common.php');
+
+$username = $_POST['username'];
+$link = $_POST['link'];
+
+$current_users = read_database();
+unset($current_users[$username]['favorites'][$link]);
+write_database($current_users);
