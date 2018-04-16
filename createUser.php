89c0aaffc7343a03dc82e3f7060e3cf2fb19703e
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-15 23:27:15 -0400

    Read username and password and enter it in database

--- createUser.php
+++ createUser.php
@@ -1,40 +1,28 @@
 <?php
 
+$user_info_file = "userInformation.json";
+
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+
+function read_json_file($filename) {
+    $json_data = file_get_contents($filename);
+    return json_decode($json_data, true);
+}
+
+function write_array_to_json_file($array, $filename) {
+    $file_contents = json_encode($array);
+    return file_put_contents($filename, $file_contents);
+}
+
 
 $user_data = array(
     'password' => $password,
     'favorites' => array()
 );
-$user_object = array(
-    $username => $user_data
-);
 
-var_dump($user_object);
+$current_users = read_json_file($user_info_file);
+$current_users[$username] = $user_data;
 
-//$DATABASE_FILE = 'userInformation.json';
-//
-//function read_database() {
-//
-//}
-//
-////Get data from existing json file
-//$jsondata = file_get_contents($myFile);
-//
-//// converts json data into array
-//$arr_data = json_decode($jsondata, true);
-//
-//// Push user data to array
-//array_push($arr_data,$formdata);
-//
-////Convert updated array to JSON
-//$jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-//
-////write json data into data.json file
-//if(file_put_contents($myFile, $jsondata)) {
-//    echo 'Data successfully saved';
-//}
-//else
-//    echo "error";
-//
+write_array_to_json_file($current_users, $user_info_file);
