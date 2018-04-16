150b73d8ca26f8e580ab475cc1d1a49a2a4063f6
Nick Montemorano <nmm9422@rit.edu>
2018-04-16 00:26:33 -0400

    Fix global variable issue in database fuctions

--- common.php
+++ common.php
@@ -3,11 +3,13 @@
 $user_info_file = "/home/fall2016/nmm9422/public_html/NewsFeed/userInformation.json";
 
 function read_database() {
+	global $user_info_file;
     $json_data = file_get_contents($user_info_file);
     return json_decode($json_data, true);
 }
 
 function write_database($array) {
+	global $user_info_file;
     $file_contents = json_encode($array);
     return file_put_contents($user_info_file, $file_contents);
 }
