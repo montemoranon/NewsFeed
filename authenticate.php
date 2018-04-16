3c2389e3025ee6c45b705b869e75a1f5522df7e2
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-16 00:36:45 -0400

    Return JSON data on authenticate

--- authenticate.php
+++ authenticate.php
@@ -7,11 +7,20 @@ $password = $_POST['password'];
 
 $users = read_database();
 
+$results = array();
 if (array_key_exists($username, $users)) {
     $user_info = $users[$username];
     $password_on_file = $user_info['password'];
 
     if($password == $password_on_file) {
-        echo "You're in."
+        $results['success'] = TRUE;
+    } else {
+        $results['success'] = FALSE;
+        $results['message'] = "Incorrect password.";
     }
+} else {
+    $results['success'] = FALSE;
+    $results['message'] = "This username does not exist in our database.";
 }
+
+echo json_encode($results);
