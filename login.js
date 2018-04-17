fc79b35ae2ecff37fb19c16a0564d56156ab49be
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-17 09:53:37 -0400

    Prevent default submission process

--- login.js
+++ login.js
@@ -1,4 +1,7 @@
 $( "#login-form" ).submit(function( event ) {
+    // stop the form from submitting normally
+    event.preventDefault();
+
     var posting = $.post( "authenticate.php", $("login-form").serialize());
 
     posting.done(function( data ) {
