e06224354f326522332a2ad05a5dbc13ad261a1b
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-17 10:00:04 -0400

    Attempt to handle form data using ajax post request

--- login.js
+++ login.js
@@ -1,10 +1,31 @@
-$( "#login-form" ).submit(function( event ) {
-    // stop the form from submitting normally
-    event.preventDefault();
+$(document).ready(function () {
+    //event handler for submit button
+    $("#login-submit").click(function () {
+        //collect userName and password entered by users
+        var userName = $("#username").val();
+        var password = $("#password").val();
 
-    var posting = $.post( "authenticate.php", $("login-form").serialize());
-
-    posting.done(function( data ) {
-      console.log(data); 
+        //call the authenticate function
+        authenticate(userName, password);
     });
-  });
+});
+
+function authenticate(userName, password) {
+    $.ajax({
+        type: "POST",
+        url: "authenticate.php",
+        success: function(data) {
+            console.log(data);
+        } 
+    })
+}
+// $( "#login-form" ).submit(function( event ) {
+//     // stop the form from submitting normally
+//     event.preventDefault();
+
+//     var posting = $.post( "authenticate.php", $("login-form").serialize());
+
+//     posting.done(function( data ) {
+//       console.log(data); 
+//     });
+//   });
