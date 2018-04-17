94cf4734ffed8d1a409bf684743fba01be5ac823
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-17 11:06:26 -0400

    Reset authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -1,26 +1,26 @@
 <?php
 
-// require('common.php');
+require('common.php');
 
-// $username = $_POST['username'];
-// $password = $_POST['password'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-// $users = read_database();
+$users = read_database();
 
-// $results = array();
-// if (array_key_exists($username, $users)) {
-//     $user_info = $users[$username];
-//     $password_on_file = $user_info['password'];
+$results = array();
+if (array_key_exists($username, $users)) {
+    $user_info = $users[$username];
+    $password_on_file = $user_info['password'];
 
-//     if($password == $password_on_file) {
-//         $results['success'] = TRUE;
-//     } else {
-//         $results['success'] = FALSE;
-//         $results['message'] = "Incorrect password.";
-//     }
-// } else {
-//     $results['success'] = FALSE;
-//     $results['message'] = "This username does not exist in our database.";
-// }
+    if($password == $password_on_file) {
+        $results['success'] = TRUE;
+    } else {
+        $results['success'] = FALSE;
+        $results['message'] = "Incorrect password.";
+    }
+} else {
+    $results['success'] = FALSE;
+    $results['message'] = "This username does not exist in our database.";
+}
 
 echo json_encode($results);
