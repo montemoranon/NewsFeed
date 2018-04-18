04ad06359fe617041528e77285ecdeae27d5c9a0
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-18 09:38:03 -0400

    Switch order of script tags in order to load jquery first

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ session_start();
 <head>
     <link rel="stylesheet" type="text/css" href="./style.css">
 
-    <script type="text/javascript" src="./newsFeed.js"></script>
-
     <!-- bootstrap 4 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
@@ -22,6 +20,9 @@ session_start();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
             crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="./newsFeed.js"></script>
+    <script type="text/javascript" src="./addFavorites.js"></script>
 
 </head>
 <body>
