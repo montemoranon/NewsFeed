81de82fdfb7825e9eb6a4a16a50b182e75a4ccc5
Nick Montemorano <nmm9422@rit.edu>
2018-04-17 21:59:50 -0400

    Fix POST request parameter name to fix login issue

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 });
 
 function authenticate(userName, password) {
-    var userInfo = {name: userName, password: password};
+    var userInfo = {username: userName, password: password};
 
     $.ajax({
         type: "POST",
