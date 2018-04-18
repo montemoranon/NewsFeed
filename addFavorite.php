12c36b7d370014055c8628e6b211b4093c4ae76f
Nicholas Montemorano <nmm9422@rit.edu>
2018-04-18 10:41:01 -0400

    Fix addFavorite.php so that a favorites list is a dictionary

--- addFavorite.php
+++ addFavorite.php
@@ -9,8 +9,7 @@ $date = $_POST['date'];
 
 // add favorite object to user
 $current_users = read_database();
-$current_users[$username]['favorites'][$link] = $title;
-$current_users[$username]['favorites']['date'] = $date;
+$current_users[$username]['favorites'][$link] = array($title, $date);
 
 write_database($current_users);
 echo json_encode($current_users);
